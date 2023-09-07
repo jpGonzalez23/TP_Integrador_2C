7bdafe47636cc310f2932879aeca11dc491643a7
Juan Pablo Gonzalez <juanpigonzalez72@gmail.com>
2023-09-06 21:15:49 -0300

    Se empieza a desarrollar las clases Numeracion y Operacion

---
+++ TP_Integrador_2C/Entidades/Modelos/Numeracion.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades.Modelos
+{
+    public class Numeracion
+    {
+        public enum ESistema
+        {
+            Binario,
+            Decimal
+        };
+
+        private ESistema sistema;
+        private double valorNumerico;
+
+        public Numeracion(double valor, ESistema sistema)
+        {
+            Numeracion.InicializarValores();
+        }
+
+        public Numeracion(string valor, ESistema sistema)
+        {
+            Numeracion.InicializarValores(valor, sistema);
+        }
+
+        private static void InicializarValores(string valor, ESistema sistema)
+        {
+
+        }
+
+        public ESistema Sistema
+        {
+            get
+            {
+                return this.sistema;
+            }
+        }
+
+        //public string Valor
+        //{
+        //    get
+        //    {
+
+        //    }
+        //}
+
+        //public static string ConvertirA(ESistema sistema)
+        //{
+
+        //}
+
+
+        public static bool operator ==(Numeracion numeroUno, Numeracion numeroDos)
+        {
+            return numeroUno.valorNumerico == numeroDos.valorNumerico;
+        }
+
+        public static bool operator !=(Numeracion numeroUno, Numeracion numeroDos)
+        {
+            return !(numeroUno == numeroDos);
+        }
+
+        public static bool operator ==(ESistema sistema, Numeracion numeracion)
+        {
+
+        }
+
+        public static bool operator !=(ESistema sistema, Numeracion numeracion)
+        {
+            return !(sistema == numeracion);
+        }
+
+        public static Numeracion operator +(Numeracion n1, Numeracion n2)
+        {
+            return n1 + n2;
+        }
+
+        public static Numeracion operator -(Numeracion n1, Numeracion n2)
+        {
+            return n1 - n2;
+        }
+
+        public static Numeracion operator *(Numeracion n1, Numeracion n2)
+        {
+            return n1 * n2;
+        }
+
+        public static Numeracion operator /(Numeracion n1, Numeracion n2)
+        {
+            return n1 / n2;
+        }
+    }
+}
---
+++ TP_Integrador_2C/Entidades/Modelos/Operacion.cs
@@ -0,0 +1,41 @@
+﻿namespace Entidades.Modelos
+{
+    public class Operacion
+    {
+        private Numeracion primerOperador;
+        private Numeracion segundoOperador;
+
+        public Numeracion PrimerOperador
+        {
+            get { return this.primerOperador; }
+            set { this.primerOperador = value; }
+        }
+
+        public Numeracion SegundoOperador
+        {
+            get { return this.segundoOperador; }
+            set { this.segundoOperador = value; }
+        }
+
+        public Operacion(Numeracion primerOperador, Numeracion segundoOperador)
+        {
+            this.primerOperador = primerOperador;
+            this.segundoOperador = segundoOperador;
+        }
+
+        public Numeracion Operar(char operacion = '+')
+        {
+            switch (operacion)
+            {
+                case '+':
+                    break;
+                case '-':
+                    break;
+                case '*':
+                    break;
+                case '/':
+                    break;
+            }
+        }
+    }
+}
